a305964f5e1742d34bad53ad4bf43b7671329fae
hamiltonc-lhs <hamiltonc@friscoisd.org>
2026-03-25 14:16:39 -0500

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,41 @@
+const subjectMappings = {
+  "CTE": [
+    "3D Model & Anim", "Accounting I", "Adv Comp Sc", "AP Comp Sci A", "AP Comp Science P", 
+    "Audio VIdeo Prod I & II", "Bus Info Mgmt", "Bus Law", "Child Dev", "Comp Sci III", 
+    "DIM", "Dollar Sense", "Entrepreneur", "Esports I", "Esports II", "Fashion Design I&II", 
+    "Floral Design I", "Food Science", "Health Science", "Intro to Cul Arts", "ISM", "Med Term", 
+    "Photojournalism", "Prac News Prod I & II Journal", "Prac News Prod I & II Newspaper", 
+    "Small Animal Mgmt", "Social Media Mktg", "Sports Mktg", "Survey Bus Mkt", "Advertising"
+  ],
+  "English": [
+    "English I", "ADV English I", "English II", "ADV English II", "English III", "AP LANG", 
+    "English IV", "AP LIT", "GT Humanities", "College Readiness", "Debate"
+  ],
+  "Math": [
+    "Algebra 1", "ADV Algebra 1", "Geometry", "ADV Geometry", "Algebra 2", "ADV Algebra 2", 
+    "Pre Calculus", "AP Pre Calculus", "AB Calculus", "BC Calculus", "Statistics", 
+    "AP Statistics", "Algebraic Reasoning", "AQR"
+  ],
+  "World Languages": [
+    "Spanish 1", "Spanish 2", "Spanish 3 Adv", "Spanish AP", "French 1", "French 2", "French 3", "French AP"
+  ],
+  "Social Studies": [
+    "GT Humanities", "World Geography", "AP Human Geography", "World History", "AP World History", 
+    "GT Amer Studies", "US History", "AP US History", "Government", "AP Government", "Econ", 
+    "AP Econ", "Psych/Sociology", "AP Psych", "AP Seminar", "AP Research"
+  ],
+  "Science": [
+    "Anatomy & Physiology", "Astronomy", "Biology", "ADV Biology", "AP Biology", "IPC", 
+    "Chemistry", "ADV Chemistry", "AP Chemistry", "Environmental Science", 
+    "AP Environmental Science (APES)", "Physics", "AP Physics 1", "AP Physics 2", "AP Physics C"
+  ],
+  "Fine Arts": [
+    "Art 1", "Art II,III, IV", "Adv Art 1", "Adv. Art 2", "AP Art 3D", "AP Art 2D", "AP Art Drawing", 
+    "AP Art History", "AP Music Theory", "Band", "Color Guard", "Dance Ens", "Dance I", "Dance II", 
+    "Dance III", "Dance IV", "Drill Team", "Orchestra", "Percussion", "Tenor/Bass Choir A", 
+    "Theatre Arts 1-4", "Tech Theatre Arts", "Theatre Prod 1-4", "Treble Choir A", "Treble Choir B"
+  ],
+  "Health & PE": [
+    "Sports Med", "PALS", "Outdoor Ed", "Health", "Life Fit Wellness", "Partners PE"
+  ]
+};
